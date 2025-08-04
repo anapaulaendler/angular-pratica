d4032d9c9e75ed8b82e1c024dd2d6c8bea4cb76f
ana paula <ana.endler@headsoft.com.br>
2025-08-04 14:22:33 -0300

    feat: task-list (click) redirects to task-details

--- angular-pratica/src/app/task/task-list/task-list.component.ts
+++ angular-pratica/src/app/task/task-list/task-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TaskStore } from '../store/task-store.service';
+import { ITask } from '../interfaces/task.interface';
 
 @Component({
   selector: 'app-task-list',
@@ -12,9 +14,13 @@ export class TaskListComponent implements OnInit {
   storeState$ = this._taskStore.storeState$;
   error$ = this._taskStore.error$;
 
-  constructor(private _taskStore: TaskStore) {}
+  constructor(private _taskStore: TaskStore, private _router: Router) {}
 
   ngOnInit(): void {
     this._taskStore.loadTasks();
+  }
+
+  goToTaskDetails(task: ITask): void {
+    this._router.navigate([`/${task.id}`]);
   }
 }
