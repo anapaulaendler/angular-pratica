41e2e1adeae841f4128c411a818ff190ddbf468b
ana paula <ana.endler@headsoft.com.br>
2025-08-06 15:17:03 -0300

    adding angular material

--- angular-pratica/src/app/app-routing.module.ts
+++ angular-pratica/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddTaskComponent } from './task/add-task/add-task.component';
 import { TaskListComponent } from './task/task-list/task-list.component';
-import { TaskDetailsComponent } from './task/task-details/task-details.component';
 
 const routes: Routes = [
   { path: '', component: TaskListComponent },
-  { path: 'new', component: AddTaskComponent },
-  { path: ':taskId', component: TaskDetailsComponent }
+  { path: 'new', component: AddTaskComponent }
 ];
 
 @NgModule({
